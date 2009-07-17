16b93ddec49b42683fcd164b3dbb83182f01e5cd
jkmassel <jkmassel@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-07-17 02:47:19 +0000

    git-svn-id: https://plugins.svn.wordpress.org/underconstruction/tags/1.02@135927 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,49 @@
+=== underConstruction ===
+Contributors: Jeremy Massel
+Donate link: http://truthmedia.com/engage/giving
+Tags: construction, under construction, private, preview, security
+Requires at least: 2.0
+Tested up to: 2.8.1
+Stable tag: 1.02
+
+Makes it so your site can only be accessed by users who log in. Useful for developing a site on a live server, without the world being able to see it
+
+== Description ==
+
+Makes it so your site can only be accessed by users who log in. Useful for developing a site on a live server, without the world being able to see it
+
+
+== Installation ==
+
+This section describes how to install the plugin and get it working.
+
+e.g.
+
+1. Upload the folder to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. If you want to change the look of the page that is displayed, click Settings->Under Construction and enter the HTML for the page you want to show up.
+
+== Frequently Asked Questions ==
+
+= What kind of HTML can I put in? =
+
+You enter the contents of the entire HTML file. You can include inline styles, or links to external style sheets and external images.
+
+= I have a question you didn't answer! =
+Post a message on the [underConstruction page](http://masseltech.com/plugins/underConstruction/ "underConstruction page") and leave a message in the comments if you have a question that needs answering!
+
+== Screenshots ==
+
+
+== Changelog ==
+
+= 1.02 =
+* Fixed a bug where clearing the text wouldn't cause it to revert to the default.
+= 1.01 =
+* Fixed a bug where deactivation would trigger an error
+= 1.0 =
+* First version
+
+== Screenshots == 
+1. The default page that is displayed (this can be overridden)
+2. The editing screen
---
+++ underConstruction.php
@@ -0,0 +1,95 @@
+<?php
+/*
+ Plugin Name: Under Construction
+ Plugin URI: http://www.masseltech.com/
+ Description: Makes it so your site can only be accessed by users who log in. Useful for developing a site on a live server, without the world being able to see it
+ Version: 1.02
+ Author: Jeremy Massel
+ Author URI: http://www.masseltech.com/
+ */
+
+/*
+ This file is part of underConstruction.
+
+    underConstruction is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    underConstruction is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with underConstruction.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+?>
+<?php
+
+add_action('get_header', 'uc_overrideWP');
+add_action('admin_menu', 'uc_adminMenu');
+register_deactivation_hook(__FILE__, 'uc_remove');
+
+function uc_overrideWP()
+{
+    if (!is_user_logged_in())
+    {
+        if (get_option('underConstructionHTML'))
+        {
+            echo ucGetHTML();
+			die();
+        }
+        else
+        {
+            require_once ('defaultMessage.php');
+			die();
+        }
+    }
+}
+
+function uc_adminMenu()
+{
+    add_options_page('Under Construction Message', 'Under Construction', 8, basename(__FILE__) , 'uc_changeMessage');
+}
+
+function uc_changeMessage()
+{
+	if(isset($_POST['ucHTML'])){
+		if(trim($_POST['ucHTML'])){
+			update_option('underConstructionHTML', attribute_escape($_POST['ucHTML']));
+		}
+		else{
+			uc_remove();
+		}
+	}
+	
+?>
+<div class="wrap">
+    <div id="icon-options-general" class="icon32">
+        <br/>
+    </div>
+    <h2>Under Construction</h2>
+    <form method="post" action="<?php echo $GLOBALS['PHP_SELF'] . '?page=underConstruction.php'; ?>">
+        <h3>Under Construction Page HTML</h3>
+		<p>Put in this area the HTML you want to show up on your front page</p>
+        <textarea name="ucHTML" rows="15" cols="75"><?php if(get_option('underConstructionHTML')){echo ucGetHTML();}?></textarea>
+     
+		<p class="submit">
+        <input type="submit" name="Submit" class="button-primary" value="Save Changes" id="submitCalendarAdd"/>
+    	</p>
+	</form>
+</div>
+<?php
+}
+
+function ucGetHTML()
+{
+	return stripslashes(html_entity_decode(get_option('underConstructionHTML')));
+}
+
+function uc_remove(){
+	delete_option('underConstructionHTML');
+}
+?>
