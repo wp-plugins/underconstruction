0a69cf40b5553507b6b135dc603e4a311e507017
jkmassel <jkmassel@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-03-16 02:46:10 +0000

    Version 1.03
    git-svn-id: https://plugins.svn.wordpress.org/underconstruction/trunk@218145 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- defaultMessage.php
+++ defaultMessage.php
@@ -1,58 +1,80 @@
-<?php
+<?php 
 /*
  This file is part of underConstruction.
-
-    underConstruction is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    underConstruction is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with underConstruction.  If not, see <http://www.gnu.org/licenses/>.
+ underConstruction is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ underConstruction is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ You should have received a copy of the GNU General Public License
+ along with underConstruction.  If not, see <http://www.gnu.org/licenses/>.
  */
-?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<title>Coming Soon</title>
-<style type="text/css">
-.comingSoon {
-	height: 92px;
-	width: 177px;
-	margin-top: 25%;
-	margin-right: auto;
-	margin-left: auto;
-	background-repeat: no-repeat;
-	color: #FFF;
-	font-size: 28px;
-	display:block;
-	text-align:center;
-}
-body,td,th {
-	font-family: Helvetica, arial, sans-serif;
-	font-size: 14px;
-}
-body {
-	margin-left: 0px;
-	margin-top: 0px;
-	margin-right: 0px;
-	margin-bottom: 0px;
-	background-color:#222222;
-}
-</style>
-</head>
 
-<body>
-<div class="comingSoon">Coming Soon
-</div>
-</body>
+function displayDefaultComingSoonPage()
+{
+    displayComingSoonPage(trim(get_bloginfo('title')).' is coming soon', get_bloginfo('url'), 'is coming soon');
+}
+
+function displayComingSoonPage($title, $headerText, $bodyText)
+{
+    
+?>
+ <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <title>
+            <?php echo $title; ?>
+        </title>
+        <style type="text/css">
+            
+            .headerText {
+                width: 550px;
+                margin-top: 10%;
+                margin-right: auto;
+                margin-left: auto;
+                font-size: 28px;
+                font-weight: normal;
+                display: block;
+                text-align: center;
+            }
+            
+            .bodyText {
+                width: 550px;
+                margin-top: 15px;
+                margin-right: auto;
+                margin-left: auto;
+                font-size: 14px;
+                font-weight: normal;
+                display: block;
+                text-align: center;
+            }
+            
+            body {
+                margin-left: 0px;
+                margin-top: 0px;
+                margin-right: 0px;
+                margin-bottom: 0px;
+                background-color: #222222;
+                color: #FFF;
+                font-family: Arial, Helvetica, sans-serif;
+            }
+        </style>
+    </head>
+    <body>
+        <span class="headerText">
+            <?php echo $headerText; ?>
+        </span>
+        <br/>
+        <span class="bodyText">
+            <?php echo $bodyText; ?>
+        </span>
+    </body>
 </html>
-<?php
+<?php 
+}
 /* EOF */
 ?>
--- underConstruction.php
+++ underConstruction.php
@@ -1,4 +1,4 @@
-<?php
+<?php 
 /*
  Plugin Name: Under Construction
  Plugin URI: http://www.masseltech.com/
@@ -10,86 +10,250 @@
 
 /*
  This file is part of underConstruction.
-
-    underConstruction is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    underConstruction is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with underConstruction.  If not, see <http://www.gnu.org/licenses/>.
+ underConstruction is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ underConstruction is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ You should have received a copy of the GNU General Public License
+ along with underConstruction.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 ?>
-<?php
-
-add_action('get_header', 'uc_overrideWP');
-add_action('admin_menu', 'uc_adminMenu');
-register_deactivation_hook(__FILE__, 'uc_remove');
-
-function uc_overrideWP()
+<?php 
+class underConstruction
 {
-    if (!is_user_logged_in())
+    var $installedFolder = "";
+    var $mainOptionsPage = "underConstructionMainOptions";
+    
+    function __construct()
     {
-        if (get_option('underConstructionHTML'))
+        $this->installedFolder = basename(dirname(__FILE__));
+    }
+    
+    function underConstruction()
+    {
+        $this->__construct();
+    }
+    
+    function getMainOptionsPage()
+    {
+        return $this->mainOptionsPage;
+    }
+    
+    function underConstructionAdminInit()
+    {
+        /* Register our script. */
+        wp_register_script('underConstructionJS', WP_PLUGIN_URL.'/'.$this->installedFolder.'/underconstruction.min.js');
+    }
+    
+    function uc_changeMessage()
+    {
+        require_once ('ucOptions.php');
+    }
+    
+    function uc_adminMenu()
+    {
+        /* Register our plugin page */
+        $page = add_options_page('Under Construction Settings', 'Under Construction', 8, $this->mainOptionsPage, array($this, 'uc_changeMessage'));
+        
+        /* Using registered $page handle to hook script load */
+        add_action('admin_print_scripts-'.$page, array($this, 'underConstructionEnqueueScripts'));
+        
+    }
+    
+    function underConstructionEnqueueScripts()
+    {
+        /*
+         * It will be called only on your plugin admin page, enqueue our script here
+         */
+        wp_enqueue_script('scriptaculous');
+        wp_enqueue_script('underConstructionJS');
+    }
+    
+    function uc_overrideWP()
+    {
+        if ($this->pluginIsActive())
         {
-            echo ucGetHTML();
-			die();
+            if (!is_user_logged_in())
+            {
+                //send a 503 if the setting requires it
+                if (get_option('underConstructionHTTPStatus') == 503)
+                {
+                    header('HTTP/1.1 503 Service Unavailable');
+                }
+                
+                if ($this->displayStatusCodeIs(0)) //they want the default!
+                {
+                    require_once ('defaultMessage.php');
+                    displayDefaultComingSoonPage();
+                    die();
+                }
+                
+                if ($this->displayStatusCodeIs(1)) //they want the default with custom text!
+                {
+                    require_once ('defaultMessage.php');
+                    displayComingSoonPage($this->getCustomPageTitle(), $this->getCustomHeaderText(), $this->getCustomBodyText());
+                    die();
+                }
+                
+                if ($this->displayStatusCodeIs(2)) //they want custom HTML!
+                {
+                    echo html_entity_decode($this->getCustomHTML());
+                    die();
+                }
+            }
+        }
+    }
+    
+    function getCustomHTML()
+    {
+        return stripslashes(get_option('underConstructionHTML'));
+    }
+    
+    function uc_remove()
+    {
+        delete_option('underConstructionHTML');
+    }
+    
+    function pluginIsActive()
+    {
+    
+        if (!get_option('underConstructionActivationStatus')) //if it's not set yet
+        {
+            return false;
+        }
+        
+        if (get_option('underConstructionActivationStatus') == 1)
+        {
+            return true;
         }
         else
         {
-            require_once ('defaultMessage.php');
-			die();
+            return false;
         }
     }
+    
+    function httpStatusCodeIs($status)
+    {
+        if (!get_option('underConstructionHTTPStatus')) //if it's not set yet
+        {
+            update_option('underConstructionHTTPStatus', 200); //set it
+        }
+        
+        if (get_option('underConstructionHTTPStatus') == $status)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+        
+    }
+    
+    function displayStatusCodeIs($status)
+    {
+        if (!get_option('underConstructionDisplayOption')) //if it's not set yet
+        {
+            update_option('underConstructionDisplayOption', 0); //set it
+        }
+        
+        if (get_option('underConstructionDisplayOption') == $status)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    
+    function getCustomPageTitle()
+    {
+        if (get_option('underConstructionCustomText') != false)
+        {
+            $fields = get_option('underConstructionCustomText');
+            return stripslashes($fields['pageTitle']);
+        }
+        else
+        {
+            return 'empty';
+        }
+    }
+    
+    function getCustomHeaderText()
+    {
+        if (get_option('underConstructionCustomText') != false)
+        {
+            $fields = get_option('underConstructionCustomText');
+            return stripslashes($fields['headerText']);
+        }
+        else
+        {
+            return 'empty';
+        }
+    }
+    
+    function getCustomBodyText()
+    {
+        if (get_option('underConstructionCustomText') != false)
+        {
+            $fields = get_option('underConstructionCustomText');
+            return stripslashes($fields['bodyText']);
+        }
+        else
+        {
+            return 'empty';
+        }
+    }
+
+    
 }
 
-function uc_adminMenu()
+$underConstructionPlugin = new underConstruction();
+
+add_action('template_redirect', array($underConstructionPlugin, 'uc_overrideWP'));
+register_deactivation_hook(__FILE__, array($underConstructionPlugin, 'uc_remove'));
+
+add_action('admin_init', array($underConstructionPlugin, 'underConstructionAdminInit'));
+add_action('admin_menu', array($underConstructionPlugin, 'uc_adminMenu'));
+
+
+function underConstructionPluginLinks($links, $file)
 {
-    add_options_page('Under Construction Message', 'Under Construction', 8, basename(__FILE__) , 'uc_changeMessage');
+    global $underConstructionPlugin;
+    if ($file == basename(dirname(__FILE__)).'/'.basename(__FILE__) && function_exists("admin_url"))
+    {
+        //add settings page
+        $manage_link = '<a href="'.admin_url('options-general.php?page='.$underConstructionPlugin->getMainOptionsPage()).'">'.__('Settings').'</a>';
+        array_unshift($links, $manage_link);
+        
+        if ($underConstructionPlugin->pluginIsActive())
+        {
+            $manage_link = '<a href="#" class="hide-if-no-js"  onclick="ajaxTurnOnUnderConstructionMode();">'.__('Turn off Under Construction Mode').'</a>';
+            $manage_link .= '<noscript><a href="'.admin_url('options-general.php?page='.$underConstructionPlugin->getMainOptionsPage().'&turnOffUnderConstructionMode').'">'.__('Turn off Under Construction Mode').'</a></noscript>';
+           
+		    array_unshift($links, $manage_link);
+        }
+        
+        if (!$underConstructionPlugin->pluginIsActive())
+        {
+            $managePageLink = 'options-general.php?page=underConstructionMainOptions';
+            $manage_link = '<a class="hide-if-no-js" href="#" onclick="ajaxTurnOnUnderConstructionMode();">'.__('Turn on Under Construction Mode').'</a>';
+            $manage_link .= '<noscript><a href="'.admin_url('options-general.php?page='.$underConstructionPlugin->getMainOptionsPage().'&turnOnUnderConstructionMode').'">'.__('Turn on Under Construction Mode').'</a></noscript>';
+            
+            array_unshift($links, $manage_link);
+        }
+
+        
+    }
+    return $links;
 }
 
-function uc_changeMessage()
-{
-	if(isset($_POST['ucHTML'])){
-		if(trim($_POST['ucHTML'])){
-			update_option('underConstructionHTML', attribute_escape($_POST['ucHTML']));
-		}
-		else{
-			uc_remove();
-		}
-	}
-	
-?>
-<div class="wrap">
-    <div id="icon-options-general" class="icon32">
-        <br/>
-    </div>
-    <h2>Under Construction</h2>
-    <form method="post" action="<?php echo $GLOBALS['PHP_SELF'] . '?page=underConstruction.php'; ?>">
-        <h3>Under Construction Page HTML</h3>
-		<p>Put in this area the HTML you want to show up on your front page</p>
-        <textarea name="ucHTML" rows="15" cols="75"><?php if(get_option('underConstructionHTML')){echo ucGetHTML();}?></textarea>
-     
-		<p class="submit">
-        <input type="submit" name="Submit" class="button-primary" value="Save Changes" id="submitCalendarAdd"/>
-    	</p>
-	</form>
-</div>
-<?php
-}
+add_filter('plugin_action_links', 'underConstructionPluginLinks', 10, 2);
 
-function ucGetHTML()
-{
-	return stripslashes(html_entity_decode(get_option('underConstructionHTML')));
-}
-
-function uc_remove(){
-	delete_option('underConstructionHTML');
-}
 ?>
